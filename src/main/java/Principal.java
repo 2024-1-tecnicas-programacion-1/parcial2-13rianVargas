c6357b7d7b97df758a1906522c645724f0e792c2
13rian Vargas <brians.vargasc@konradlorenz.edu.co>
2024-06-06 09:55:11 -0500

    Parcial II

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,5 +1,12 @@
+import parcial2.Automovil;
+import parcial2.Motocicleta;
+import parcial2.Vehiculo;
+
 public class Principal {
     public static void main(String[] args) {
-        // TODO: Tu código va aquí
+        Vehiculo Vehiculo;
+        Automovil Automovil;
+        Motocicleta Motocicleta;
+        
     }
 }
--- src/main/java/parcial2/Motocicleta.java
+++ src/main/java/parcial2/Motocicleta.java
@@ -1,5 +1,23 @@
 package parcial2;
 
-public class Motocicleta {
-    // TODO: Tu código va aquí
+public class Motocicleta extends Vehiculo{
+    private TipoMotocicleta tipoMotocicleta;
+
+    public Motocicleta(String marca, String modelo, int año, TipoMotocicleta tipoMotocicleta) {
+        super(marca, modelo, año);
+        this.tipoMotocicleta = tipoMotocicleta;
+    }
+
+    public TipoMotocicleta getTipoMotocicleta() {
+        return tipoMotocicleta;
+    }
+
+    public void setTipoMotocicleta(TipoMotocicleta tipoMotocicleta) {
+        this.tipoMotocicleta = tipoMotocicleta;
+    }
+
+    public String imprimirInformacion() {
+        return getMarca() + " " + getModelo() + " " + getAño() + ", tipo de motocicleta: " + getTipoMotocicleta();
+    }
+    
 }
