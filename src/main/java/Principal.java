f3192fc79c61d96818b03cc23d7338faddde7ab0
13rian Vargas <brians.vargasc@konradlorenz.edu.co>
2024-06-06 10:10:53 -0500

    Parcial II Implementación de Main

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,12 +1,17 @@
 import parcial2.Automovil;
 import parcial2.Motocicleta;
+import parcial2.TipoCombustible;
+import parcial2.TipoMotocicleta;
 import parcial2.Vehiculo;
 
 public class Principal {
     public static void main(String[] args) {
-        Vehiculo Vehiculo;
-        Automovil Automovil;
-        Motocicleta Motocicleta;
+        Vehiculo Vehiculo = new Vehiculo("Niu", "Qi3", 2020); 
+        Automovil Automovil = new Automovil("Tesla", "X", 2024, 4, TipoCombustible.ELECTRICO);
+        Motocicleta Motocicleta = new Motocicleta("Yamaha", "R3", 2023, TipoMotocicleta.DEPORTIVA);
         
+        System.out.println(Vehiculo.imprimirInformacion());
+        System.out.println(Automovil.imprimirInformacion());
+        System.out.println(Motocicleta.imprimirInformacion());
     }
 }
